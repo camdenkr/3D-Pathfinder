f3ca7bf3baf095fed927f2dd3f6bb010e06bbe7f
Woody_Wan <woodyallen1999@live.com>
2021-12-01 15:26:42 -0500

    Test code of 3D click

---
+++ source/testmap.m
@@ -0,0 +1,162 @@
+clc;
+clear;
+
+ptCloud = pcread('trashcan.ply');%Read Pointcloud
+
+figure(1);
+pcshow(ptCloud);
+xlabel('X(m)');
+ylabel('Y(m)');
+zlabel('Z(m)');
+title('Original Point Cloud');
+
+maxDistance = 0.02;
+
+referenceVector = [0,0,1];
+
+maxAngularDistance = 5;
+
+limx = get(gca, 'XLim');
+limy = get(gca, 'YLim');
+
+%Get coordinates in pointcloud
+keydown = 2;
+view(0,90);
+while keydown ~= 0
+   disp('Click some where on the figure');
+   keydown = waitforbuttonpress;
+end
+
+currPt = get(gca,'CurrentPoint');
+disp(currPt);
+hold on;
+plot3( currPt(:,1), currPt(:,2), currPt(:,3), 'k-', 'LineWidth', 2);
+view(0,90);  %rotate to view this line
+xyz1 = evalin('base','currPt')
+
+keydown = 2;
+
+while keydown ~= 0
+   disp('Click some where on the figure');
+   keydown = waitforbuttonpress;
+end
+
+currPt = get(gca,'CurrentPoint');
+disp(currPt);
+hold on;
+plot3( currPt(:,1), currPt(:,2), currPt(:,3), 'k-', 'LineWidth', 2);
+view(0,90);  %rotate to view this line
+xyz2 = evalin('base','currPt')
+
+% Get point cloud without floor or ceiling
+[model1,inlierIndices,outlierIndices] = pcfitplane(ptCloud,...
+            maxDistance,referenceVector,maxAngularDistance);
+plane1 = select(ptCloud,inlierIndices);
+remainPtCloud = select(ptCloud,outlierIndices);
+[plane_bins,bins] = pcbin(plane1,[128 128 1]);
+figure(2);
+fig = figure(2);
+pcshow(plane1);
+title('Ground');
+
+
+
+figure(3);
+pcshow(remainPtCloud);
+title('Remaining Point Cloud');
+
+% Generate 3D occupancy grid of the remaining point cloud after floor and
+% ceiling removed
+indices = pcbin(remainPtCloud,[128 128 128]);
+
+occupancyGrid = cellfun(@(c) ~isempty(c), indices);
+
+ViewPnl = uipanel(figure);
+volshow(occupancyGrid, 'Parent' ,ViewPnl);
+
+% Generate 2D Occupancy Grid/Density grid (same use case), flattening 3D
+% space into occupied and non-occupied cells
+indices = pcbin(remainPtCloud,[128 128 1]);
+
+densityGrid = cellfun(@(c) ~isempty(c),indices);
+
+figure;
+imagesc(densityGrid);
+
+%Set up and get the cordinates of 2 points
+x = zeros(2);
+y = zeros(2);
+
+x(1) = (xyz1(1,1) - limx(1))/(limx(2) - limx(1))*128;
+x(2) = (xyz2(1,1) - limx(1))/(limx(2) - limx(1))*128;
+y(1) = (xyz1(1,2) - limy(1))/(limy(2) - limy(1))*128;
+y(2) = (xyz2(1,2) - limy(1))/(limy(2) - limy(1))*128;
+
+x = int8(x);
+y = int8(y);
+
+MAP = densityGrid;
+OffsteMap = MAP;
+[row,col] = find(MAP == 1);
+Barrierpoint = [row,col];
+
+[r,c] = size(Barrierpoint);
+% Add Offset to the Barriers
+for i = 1:r
+       m = Barrierpoint(i,1);
+       n = Barrierpoint(i,2);
+       if (1 == m) && (1 < n) && (128 > n)
+           OffsteMap(m:m + 1,n - 1:n + 1) = ones(2,3);    
+       elseif (1 == n) && (1 < m) && (128 > m)
+           OffsteMap(m - 1:m + 1,n:n + 1) = ones(3,2);
+       elseif (128 == m) && (128 > n)
+           OffsteMap(m - 1:m,n - 1:n + 1) = ones(2,3);
+       elseif (128 == n) && (128 > m)
+           OffsteMap(m - 1:m + 1,n - 1:n) = ones(3,2);    
+       elseif (128 == n) && (128 == m)
+           OffsteMap(m - 1:m,n - 1:n + 1) = ones(2,2);
+       elseif (1 == n) && (1 == m)
+           OffsteMap(m:m + 1,n:n + 1) = ones(2,2);    
+       else
+           OffsteMap(m - 1:m + 1,n - 1:n + 1) = ones(3,3);
+       end
+end
+
+
+StartX = x(1);
+StartY = y(1);
+
+GoalRegister = int8(zeros(128,128));
+GoalRegister(y(2),x(2))=1;
+
+Connecting_Distance=8; %Avoid to high values Connecting_Distances for reasonable runtimes. 
+% Running PathFinder
+OptimalPath = ASTARPATH(StartX,StartY,OffsteMap,GoalRegister,Connecting_Distance)
+% End. 
+% Plot optimal path
+if size(OptimalPath,2)>1
+hold on
+plot(OptimalPath(1,2),OptimalPath(1,1),'o','color','k')
+plot(OptimalPath(end,2),OptimalPath(end,1),'o','color','b')
+plot(OptimalPath(:,2),OptimalPath(:,1),'r')
+legend('Goal','Start','Path')
+else 
+     pause(1);
+ h=msgbox('Sorry, No path exists to the Target!','warn');
+ uiwait(h,5);
+end
+
+
+figure()
+
+% For each bin in the optimal path, take all the points in the
+% corresponding bin of the floor ponitcloud and turn them red
+for i = 1:length(OptimalPath)
+    bin = cell2mat(plane_bins(OptimalPath(i,1), OptimalPath(i,2)));
+    for j = 1:length(bin)
+       plane1.Color(bin(j),:) = [255,0,0];
+       plane1.Color(bin(j)+1,:) = [255,0,0];
+       plane1.Color(bin(j)-1,:) = [255,0,0];
+    end
+end
+pcshow(plane1);
